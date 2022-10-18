ea38f38d2d251dde3077fe806518ca818c296e31
David Hontecillas <dhontecillas@gmail.com>
2022-10-18 23:02:40 +0200

    add upload static files from a deployed app

---
+++ ansible/app/depl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# check if the old way of executing still works, or some parts are not needed anymore
+# ansible-playbook -i hosts/$1 server_setup.yaml -e 'ansible_python_interpreter=/usr/bin/python3' -K --become-method=su
+ansible-playbook -i $1 app_setup.yaml --extra-vars "@$2" -K --become-method=su
