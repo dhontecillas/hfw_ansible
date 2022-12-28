b5f6e0fc7f096a3e68e9f2fd2b77668323a5fcb9
David Hontecillas <dhontecillas@gmail.com>
2022-12-28 15:15:12 +0100

    improve env.sh script

--- env.sh
+++ env.sh
@@ -1,1 +1,17 @@
-. ./venv/bin/activate
+#!/bin/bash
+
+if [[ "$VIRTUAL_ENV" ]]
+then
+    echo "enviroment already set $VIRTUAL_ENF"
+else
+    if [[ ! -d "venv" ]]
+    then
+        python3 -m venv venv
+        . ./venv/bin/activate
+        pip install --upgrade pip
+        pip install -r ./requirements.pip
+    else
+        . ./venv/bin/activate
+    fi
+fi
+
